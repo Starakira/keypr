f934840dff709ad4791dbd1bc76fb5985aad68b9
Angelica Irene Christina <airenech@gmail.com>
2020-06-01 15:45:04 +0700

    Add Script Settings

---
+++ Challenge3/Scripts Screen/ScriptSetting/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Challenge3
+//
+//  Created by Angelica Irene Christina on 01/06/20.
+//  Copyright © 2020 joahan wirasugianto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
